fdd89d8e6ce279e2e64c302dc1dd270890285340
Gabriel Santana <gabsop@hotmail.com>
2020-06-13 00:11:15 -0300

    Foi

--- nlw/scripts/create-point.js
+++ nlw/scripts/create-point.js
@@ -21,12 +21,15 @@ function getCities(event) {
 
     const ufValue = event.target.value
 
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.disabled = true
+
     fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`)
     .then( res => res.json())
     .then( cities => {
-
+        
         for(city of cities) {
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
         }
         
         citySelect.disabled = false
@@ -38,3 +41,44 @@ function getCities(event) {
 document
     .querySelector("select[name=uf]")
     .addEventListener("change", getCities)
+
+
+//Itens de coleta
+const itemstoCollect = document.querySelectorAll(".items-grid li")
+
+for (item of itemstoCollect){
+    item.addEventListener("click", handleSelectedItem)
+}
+
+const collectedItems = document.querySelector("input[name=items]")
+
+let selectedItems = []
+
+function handleSelectedItem(event) {
+    const itemLi = event.target
+
+    //adicionar ou removar uma classe com javascript
+    itemLi.classList.toggle("selected")
+    
+    const itemId = itemLi.dataset.id
+
+    //verificar se existem itens selecionados,
+    //se sim, pegar os itens
+
+    const alreadySelected = selectedItems.findIndex(item => item == itemId)
+
+
+    //se já estiver selecionado
+    if(alreadySelected >= 0) {
+        //tirar da seleção
+        const filteredItems = selectedItems.filter(item => item != itemId)
+        selectedItems = filteredItems
+    } else {
+        // se não estiver selecionado, adicionar à seleção
+        selectedItems.push(itemId)
+    }
+
+    //atualizar o campo escondido com os itens selecionados
+    collectedItems.value = selectedItems
+
+}
