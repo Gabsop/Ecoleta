ca65f757112019bae9e5cc170297db3bb2ecce91
Gabriel Santana <gabsop@hotmail.com>
2020-06-13 01:19:55 -0300

    Rotas feitas

---
+++ nlw/src/server.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const server = express()
+
+//Configurar pasta pública
+server.use(express.static("public"))
+
+//Configurar caminhos da aplicação
+//Página inicial
+//req: Requisição
+//res: Resposta
+server.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html")
+})
+
+server.get("/create-point", (req, res) => {
+    res.sendFile(__dirname + "/views/create-point.html")
+})
+
+
+
+//Ligar o servidor
+server.listen(3000)
