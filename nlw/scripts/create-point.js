c4b3c4e64c547e4f775cb69f3703d487705311ba
Gabriel Santana <gabsop@hotmail.com>
2020-06-12 23:29:26 -0300

    Agora foi

--- nlw/scripts/create-point.js
+++ nlw/scripts/create-point.js
@@ -1,19 +1,40 @@
-    
- function populateUfs () {
-    const ufSelect = document.querySelector("select[name=uf]]")
+    function populateUfs () {
+   const ufSelect = document.querySelector("select[name=uf]")
     
     fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
-    .then(res => res.json())
-    .then(states => ){
+    .then(res => res.json() )
+    .then(states => {
         for(state of states){
             ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
         }
     })
  }
 
- populateUfs()
+populateUfs()
+
+function getCities(event) {
+    const citySelect = document.querySelector("select[name=city]")
+    const stateInput = document.querySelector("input[name=state]")
+
+    const indexOfSelectedState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectedState].text
+
+    const ufValue = event.target.value
+
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`)
+    .then( res => res.json())
+    .then( cities => {
+
+        for(city of cities) {
+            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+        }
+        
+        citySelect.disabled = false
+    })
+}
 
 
-// document
-//     .querySelector("select[name=uf]")
-//     .addEventListener("change", () => {})
+
+document
+    .querySelector("select[name=uf]")
+    .addEventListener("change", getCities)
